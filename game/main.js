21c1df319e88d4684453bbb7412478e7356389ce
jungeun <shahmaran0207@naver.com>
2024-03-29 18:16:02 +0900

    second_start

---
+++ game/main.js
@@ -0,0 +1,30 @@
+import { Bodies, Engine, Render, Runner, World } from "matter-js";
+
+const engine = Engine.create();
+const render = Render.create({
+  engine,
+  element: document.body,
+  options: {
+    wireframes: false,
+    background: "#F7F4C8",
+    width: 620,
+    height: 850,
+  }
+});
+
+const world = engine.world;
+
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
+});
+
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
+});
+
+  World.add(world, [leftWall, rightWall]);
+
+  Render.run(render);
+  Runner.run(engine);
