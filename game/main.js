ac012072512ee024db3e8e20f885f8147d39e8de
jungeun <shahmaran0207@naver.com>
2024-03-29 18:59:30 +0900

    game

--- game/main.js
+++ game/main.js
@@ -1,4 +1,5 @@
 import { Bodies, Engine, Render, Runner, World } from "matter-js";
+import {FRUITS } from "./fruits";
 
 const engine = Engine.create();
 const render = Render.create({
@@ -24,7 +25,29 @@ const rightWall = Bodies.rectangle(605, 395, 30, 790, {
   render: { fillStyle: "#E6B143" }
 });
 
-  World.add(world, [leftWall, rightWall]);
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
+});
+
+const topline  = Bodies.rectangle(310, 150, 620, 2, {
+  isStatic: true,
+  render: {fillStyle:"#E6B143"}
+});
+
+  World.add(world, [leftWall, rightWall, ground, topline]);
 
   Render.run(render);
   Runner.run(engine);
+
+  function addFruit(){
+    const index=7;
+    const fruit =FRUITS[index];
+
+    const body=Bodies.circle(300, 50, fruit.radius, {
+      index: index,
+      render: {
+        sprite:{ texture: `${fruit.name}.png`} 
+      }
+    })
+  }
