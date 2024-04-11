ea3d63efc7e9cfc50aeefaffdcf6bf1ea2041187
jungeun <shahmaran0207@naver.com>
2024-04-11 18:17:01 +0900

    updating_area

---
+++ final_project/springversion/rev/src/main/java/Service/Service.java
@@ -0,0 +1,31 @@
+package Service;
+
+@org.springframework.stereotype.Service
+
+public class Service {
+	public int money() {
+		
+		return 0;
+	}
+
+}
+
+
+
+
+//@Service
+//public class EX02Service {
+//	
+//	AccountVO acc=new AccountVO("itbank", "it", "아뱅");
+//	
+//	public String login(AccountVO input) {
+//		String msg="로그인 실팽";
+//		
+//		if(acc.equals(input)) {
+//			msg=acc.getNick()+"님 로그인 성공";
+//		}
+//		
+//		return msg;
+//	}
+//
+//}
