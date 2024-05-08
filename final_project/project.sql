118a32f11779021729f6e2cc4ccfa5db90763310
jungeun <shahmaran0207@naver.com>
2024-05-08 19:54:42 +0900

    file_complete

--- final_project/project.sql
+++ final_project/project.sql
@@ -1,3 +1,6 @@
+create user C##PROJECT identified by 1;
+grant dba, resources, connect to C##PROJECT;
+
 create table area(
     predict_month         varchar2(200),
     predict_value           NUMBER
